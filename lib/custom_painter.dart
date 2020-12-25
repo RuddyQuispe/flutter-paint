28d99d0856dbe267c1a074f5043821f50cd935d3
RuddyQuispe <ruddyq18@gmail.com>
2020-12-25 01:58:34 -0400

    build: create CustomPainter

---
+++ lib/custom_painter.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyCustomPainter extends CustomPainter {
+  List<Offset> points;
+  Color color;
+  double stroke;
+
+  MyCustomPainter({this.points, this.color, this.stroke});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint background = Paint()..color = Colors.white;
+    Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
+    canvas.drawRect(rect, background);
+    Paint paint = Paint();
+    paint.color = Colors.black;
+    paint.strokeWidth = 2.0;
+    paint.isAntiAlias = true;
+    paint.strokeCap = StrokeCap.round;
+    for (var i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i], points[i + 1], paint);
+      } else if (points[i] != null && points[i + 1] != null) {
+        canvas.drawPoints(PointMode.points, [points[i]], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    throw UnimplementedError();
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:paint_flutter_app/custom_painter.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
@@ -8,8 +10,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Offset> points;
+  Color selectedColor;
+  double strokeWidth;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedColor = Colors.black;
+    strokeWidth = 2.0;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Stack(
         children: [
@@ -23,9 +38,113 @@ class _MyHomePageState extends State<MyHomePage> {
                   Color.fromRGBO(233, 64, 87, 1.0),
                   Color.fromRGBO(242, 113, 33, 1.0)
                 ])),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: width * 0.80,
+                  height: height * 0.80,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            blurRadius: 5.0,
+                            spreadRadius: 1.0),
+                      ]),
+                  child: GestureDetector(
+                    onPanDown: (details) {
+                      this.setState(() {
+                        points.add(details.localPosition);
+                      });
+                    },
+                    onPanUpdate: (details) {
+                      this.setState(() {
+                        points.add(details.localPosition);
+                      });
+                    },
+                    onPanEnd: (details) {
+                      this.setState(() {
+                        points.add(null);
+                      });
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      child: CustomPaint(
+                        painter: MyCustomPainter(
+                            points: points,
+                            color: selectedColor,
+                            stroke: strokeWidth),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: width * 0.80,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                  child: Row(
+                    children: [
+                      IconButton(
+                          icon:
+                              Icon(Icons.color_lens, color: this.selectedColor),
+                          onPressed: () => selectColor()),
+                      Expanded(
+                          child: Slider(
+                        min: 1.0,
+                        max: 10,
+                        activeColor: selectedColor,
+                        value: strokeWidth,
+                        onChanged: (value) {
+                          this.setState(() {
+                            strokeWidth = value;
+                          });
+                        },
+                      )),
+                      IconButton(
+                          icon: Icon(Icons.layers_clear),
+                          onPressed: () => this.setState(() {
+                                points.clear();
+                              })),
+                    ],
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
     );
+  }
+
+  void selectColor() {
+    showDialog(
+        context: context,
+        child: AlertDialog(
+          title: const Text("Select a Color"),
+          content: SingleChildScrollView(
+            child: BlockPicker(
+              pickerColor: selectedColor,
+              onColorChanged: (value) => this.setState(() {
+                this.selectedColor = value;
+              }),
+            ),
+          ),
+          actions: [
+            FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("Close"))
+          ],
+        ));
   }
 }
